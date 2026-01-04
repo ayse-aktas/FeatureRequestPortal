f967b4c69b02c33535d96a2ef6fd407d907de01b
Ayse Aktas <ayse.aktas8@ogr.sakarya.edu.tr>
2026-01-05 01:22:06 +0300

    feat: define IFeatureRequestAppService interface

---
+++ src/FeatureRequestPortal.Application.Contracts/FeatureRequests/IFeatureRequestAppService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace FeatureRequestPortal.FeatureRequests;
+
+public interface IFeatureRequestAppService : IApplicationService
+{
+    Task<FeatureRequestDto> GetAsync(Guid id);
+
+    Task<PagedResultDto<FeatureRequestDto>> GetListAsync(FeatureRequestGetListInput input);
+
+    Task<FeatureRequestDto> CreateAsync(CreateFeatureRequestDto input);
+
+    Task<FeatureRequestDto> UpdateAsync(Guid id, UpdateFeatureRequestDto input);
+
+    Task DeleteAsync(Guid id);
+
+    Task VoteAsync(Guid id);
+
+    Task<CommentDto> CreateCommentAsync(CreateCommentDto input);
+}
