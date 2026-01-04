b6e36cdd8cdfab325c64c3637c33a3214debd2a3
Ayse Aktas <ayse.aktas8@ogr.sakarya.edu.tr>
2026-01-05 00:49:59 +0300

    Create base ABP application setup

---
+++ src/FeatureRequestPortal.Domain/FeatureRequestPortalConsts.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Identity;
+
+namespace FeatureRequestPortal;
+
+public static class FeatureRequestPortalConsts
+{
+    public const string DbTablePrefix = "App";
+    public const string? DbSchema = null;
+    public const string AdminEmailDefaultValue = IdentityDataSeedContributor.AdminEmailDefaultValue;
+    public const string AdminPasswordDefaultValue = IdentityDataSeedContributor.AdminPasswordDefaultValue;
+}
---
+++ src/FeatureRequestPortal.EntityFrameworkCore/EntityFrameworkCore/FeatureRequestPortalDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FeatureRequestPortal.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class FeatureRequestPortalDbContextFactory : IDesignTimeDbContextFactory<FeatureRequestPortalDbContext>
+{
+    public FeatureRequestPortalDbContext CreateDbContext(string[] args)
+    {
+        var configuration = BuildConfiguration();
+        
+        FeatureRequestPortalEfCoreEntityExtensionMappings.Configure();
+
+        var builder = new DbContextOptionsBuilder<FeatureRequestPortalDbContext>()
+            .UseSqlite(configuration.GetConnectionString("Default"));
+        
+        return new FeatureRequestPortalDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../FeatureRequestPortal.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables();
+
+        return builder.Build();
+    }
+}
